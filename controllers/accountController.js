da3e32da33cc9a3629b3834a090f22f24e41c0ef
joshrmorris2 <moofase1@gmail.com>
2024-03-04 21:24:43 -0800

    password hashing

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,5 +1,6 @@
 const utilities = require("../utilities/")
 const accountModel = require('../models/account-model')
+const bcrypt = require('bcryptjs')
 
 /* ****************************************
 *  Deliver login view
@@ -32,11 +33,25 @@ async function registerAccount(req, res) {
     let nav = await utilities.getNav()
     const { account_firstname, account_lastname, account_email, account_password } = req.body
 
+    // Hash password
+    let hashedPassword
+    try {
+        //regular password and cost
+        hashedPassword = await bcrypt.hashSync(account_password, 10)
+    } catch (error) {
+        req.flash('notice', 'Sorry, there was an error processing the registration')
+        res.status(500).render('account/register', {
+            title: 'Register',
+            nav,
+            errors: null,
+        })
+    }
+
     const regResult = await accountModel.registerAccount(
         account_firstname,
         account_lastname,
         account_email,
-        account_password
+        hashedPassword
     )
   
     if (regResult) {
@@ -47,6 +62,7 @@ async function registerAccount(req, res) {
         res.status(201).render("account/login", {
             title: "Login",
             nav,
+            errors: null,
       })
     } else {
         req.flash("notice", "Sorry, the registration failed.")
